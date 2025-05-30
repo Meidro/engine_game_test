ad4f15839241dfd76589943506b688e8f0892acf
Minibaev Evgeniy <meidro@yandex.ru>
2025-05-30 21:34:06 +0300

    Loader

--- src/engine/Loader.ts
+++ src/engine/Loader.ts
@@ -1,15 +1,76 @@
 export class Loader {
-  constructor() {}
+  loadOrder: {
+    dataImagesList: {[key: string]: string};
+    dataJsonsList: {[key: string]: string};
+  };
+  resources: {
+    imagesList: {[key: string]: HTMLImageElement};
+    jsonsList: {[key: string]: JSON};
+  };
+
+  constructor() {
+    this.loadOrder = {
+      dataImagesList: {},
+      dataJsonsList: {},
+    };
+    this.resources = {
+      imagesList: {},
+      jsonsList: {},
+    };
+  }
+
+  addDataImageOrderLoad(name: string, src: string) {
+    this.loadOrder.dataImagesList[name] = src;
+  }
+
+  addDataJsonOrderLoad(name: string, address: string) {
+    this.loadOrder.dataJsonsList[name] = address;
+  }
+
+  load(callback: () => void) {
+    const promises = [];
+    const dataImagesList = this.loadOrder.dataImagesList;
+    const dataJsonsList = this.loadOrder.dataJsonsList;
+
+    for (let key in dataImagesList) {
+      const promise = Loader.loadImage(dataImagesList[key]).then((image) => {
+        this.resources.imagesList[key] = image;
+        delete dataImagesList[key];
+      });
+
+      promises.push(promise);
+    }
+
+    for (let key in dataJsonsList) {
+      const promise = Loader.loadJson(dataJsonsList[key]).then((json) => {
+        this.resources.jsonsList[key] = json;
+        delete dataJsonsList[key];
+      });
+
+      promises.push(promise);
+    }
+
+    Promise.all(promises).then(callback);
+  }
 
   static loadImage(src: string): Promise<HTMLImageElement> {
     return new Promise((resolve, reject) => {
       try {
         const image: HTMLImageElement = new Image();
-        image.onload = () => resolve(image);
         image.src = src;
+        image.onload = () => resolve(image);
       } catch (error) {
         reject(error);
       }
+    });
+  }
+
+  static loadJson(address: string): Promise<JSON> {
+    return new Promise((resolve, reject) => {
+      fetch(address)
+        .then((result) => result.json())
+        .then((json) => resolve(json))
+        .catch((error) => reject(error));
     });
   }
 }
--- src/game/index.ts
+++ src/game/index.ts
@@ -1,5 +1,11 @@
 import {Loader} from '../engine/Loader.js';
 
-Loader.loadImage('../static/rabbit.jpg').then((image) => {
-  document.body.append(image);
+const loader = new Loader();
+
+loader.addDataImageOrderLoad('rabbit', '../static/rabbit.jpg');
+loader.addDataJsonOrderLoad('persons', '../static/persons.json');
+loader.load(() => {
+  console.log('fired!');
 });
+
+console.log(loader);
